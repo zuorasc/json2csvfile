05fe1aac24756e893a1fdc85c02e33cf40dfa116
John Henry <john.henry@zuora.com>
2014-01-09 11:57:55 -0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+var argv = require('optimist').argv;
+var fs = require("fs");
+try{
+	var inFile = argv._[0];
+	var outFile = argv._[1];
+	if(!inFile || !outFile){
+		throw new Error("Arguments not properly defined.")
+	}
+	var json = JSON.parse(fs.readFileSync(inFile).toString());
+	var keys = Object.keys(json[0]);
+	var file = fs.createWriteStream(outFile);
+	file.once('open',function(){
+		file.write(keys.join(',') + '\n');
+		for(var i = 0; i < json.length; i++){
+			var row = [];
+			for(var j = 0; j < keys.length; j++){
+				row.push(json[i][keys[j]]);
+			}
+			file.write(row.join(',')+'\n');
+		}
+		file.end();
+		console.log(outFile + " successfully written. You're welcome ;)")
+	})
+}catch(error){
+	console.log(error.message)
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+#json2csvfile
+
+##Convert JSON file to CSV.
+
+## Installation
+`npm install -g json2csvfile`
+npm
+
+##Usage
+`json2csvfile infile.json outfile.csv`
